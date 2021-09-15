501715c4b0d01310a5395a3107ed8361fd3e4407
Jay Young <jyoungta@cisco.com>
2021-09-15 12:23:20 -0400

    first upload of the windows script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # CiscoTacFirstResponder-Anyconnect
-Cisco TAC First Responder script for Anyconnect.  The hosted scripts streamline generation and transmission of diagnostic (DART) bundles from a user's computer to Cisco TAC.
+
+Cisco TAC First Responder script for Anyconnect.
+
+The hosted scripts streamline generation and transmission of diagnostic (DART) bundles from a user's computer to Cisco TAC.
+
+For Windows have the end user open PowerShell on their computer and paste the in following (with the `$CxdUsername` and `$CxdPassword` replaced with the value provided by your Cisco TAC Engineer.)
+
+```powershell
+$CxdToken = 'mkDOLk2YO9SyuEkz'
+$CxdUsername = '611111111'
+Invoke-Expression -Command ((Invoke-WebRequest -uri "https://raw.githubusercontent.com/CiscoCX/CiscoTacFirstResponder-Anyconnect/main/anyconnect-windows.ps1" -UseBasicParsing).Content)
+```
+
+Example output:
+
+```
+Put here the example output
+
+```
---
+++ anyconnect-windows.ps1
@@ -0,0 +1,55 @@
+# This script will call Cisco Anyconnect DART Program on the user computer
+# to generate a DART Bundle.  Then using the CxdUsername and CsxToken values
+# provided by a Cisco TAC engineer it will upload the file to Cisco TAC
+
+function Send-CiscoTacFile {
+    # Sends the file via https to Cisco TAC using the 
+    # username and password provided by the TAC Engineer
+    param (
+        [string] $CxdUsername,
+        [string] $CxdToken,
+        [string] $FilePath
+    )
+    $credBytes = [System.Text.Encoding]::UTF8.GetBytes(
+        ('{0}:{1}' -f $CxdUsername, $CxdToken)
+    )
+    $cred = [Convert]::ToBase64String($credBytes)
+    $Authorization = 'Basic {0}' -f $cred
+    $Headers = @{ Authorization = $Authorization }
+    $Filename = Split-Path $FilePath -Leaf 
+    $Uri = 'https://cxd.cisco.com/home/{0}' -f $Filename
+    Invoke-WebRequest -Uri $Uri -InFile $FilePath -Headers $Headers -Method Put -useBasicParsing
+}
+
+function New-DartBundle {
+    # Generates the DART bundle with the current datetime in the filename
+    # and puts it on the user's desktop
+    $DesktopPath = [Environment]::GetFolderPath("Desktop")
+    $DartLocation = '{0}\DARTBundle_{1}.zip' -f $DesktopPath, (Get-Date -Format FileDateTimeUniversal)
+    $DartCliLocation = 'C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\DART\dartcli.exe'
+    Start-Process -FilePath $DartCliLocation -ArgumentList "-dst `"$DartLocation`"" -Wait -NoNewWindow
+    return $DartLocation
+}
+
+Write-Output "+-------------------------------------------------------------------------+"
+Write-Output "| Welcome to the PowerShell Based Cisco AnyConnect DART Collection Script |"
+Write-Output "|                                                                         |"
+Write-Output "|  For issues with the collection script/process please file an issue at  |"
+Write-Output "|   https://github.com/CiscoCX/CiscoTacFirstResponder-Anyconnect/issues   |"
+Write-Output "|                                                                         |"
+Write-Output "+-------------------------------------------------------------------------+"
+Write-Output ""
+Write-Output "This script will initiate a collection of the Cisco AnyConnect DART Bundle and then upload it directly to Cisco TAC"
+
+if (!$CxdUsername -or !$CxdToken) {
+    Write-Output ""
+    Write-Output "Error: Both the `$CxdUsername and `$CxdToken Variable must be set before executing this script.  Please contact your Cisco TAC Engineer to obtain the values".
+    Return
+}
+
+Write-Output "Attempting to collect the DART Bundle"
+$CollectedFilePath = New-DartBundle
+Write-Output "File collected at $CollectedFilePath"
+Write-Output "Attempting to transmit to DART Bundle to Cisco TAC for SR $cxdUsername"
+Send-CiscoTacFile -CxdUsername $CxdUsername -CxdToken $CxdToken -FilePath $CollectedFilePath
+Write-Output "Collection Complete!"
