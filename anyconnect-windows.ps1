bcdc559d3738789c34783eb86499e990a9fdc91e
Jay Young <80918493+ministryofjay-cisco@users.noreply.github.com>
2021-09-29 10:18:31 -0400

    Cisco Secure Client changed its location
    Basic checking of the presence of the dart executable

--- anyconnect-windows.ps1
+++ anyconnect-windows.ps1
@@ -26,7 +26,17 @@ function New-DartBundle {
     # and puts it on the user's desktop
     $DesktopPath = [Environment]::GetFolderPath("Desktop")
     $DartLocation = '{0}\DARTBundle_{1}.zip' -f $DesktopPath, (Get-Date -Format FileDateTimeUniversal)
-    $DartCliLocation = 'C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\DART\dartcli.exe'
+    $AnyconnectDartCliLocation = 'C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\DART\dartcli.exe'
+    $SecureClientDartCliLocation = 'C:\Program Files (x86)\Cisco\Cisco Secure Client\DART\dartcli.exe'
+    if (Test-Path -Path $AnyconnectDartCliLocation -PathType Leaf) {
+        $DartCliLocation = $AnyconnectDartCliLocation
+    }
+    if (Test-Path -Path $SecureClientDartCliLocation -PathType Leaf) {
+        $DartCliLocation = $SecureClientDartCliLocation
+    }
+    if (-not($DartCliLocation)) {
+        throw "Unable to find DART Program.  Is DART installed?"
+    }
     Start-Process -FilePath $DartCliLocation -ArgumentList "-dst `"$DartLocation`"" -Wait -NoNewWindow
     return $DartLocation
 }
